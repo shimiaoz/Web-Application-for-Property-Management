a94a8af510745812c90ea4ab2d3b18faada16ab1
shimiaoz <36020011+shimiaoz@users.noreply.github.com>
2018-05-18 23:28:39 -0400

    Add files via upload

--- login.php
+++ login.php
@@ -1,31 +1,13 @@
 <?php
-// Start the session
 session_start();
-
 require 'dbinfo.php';
 
-$email = $password = "";
-$emailErr = $passwordErr = "";
+$email = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["email"])) {
-        $emailErr = "Email is required";
-    }
-    else {
-        $email = $_POST["email"];
-    }
-
-    if (empty($_POST["password"])) {
-        $passwordErr = "Password is required";
-    }
-    else {
-        $password = $_POST["password"];
-    }
-}
-
-if(!empty($_POST['email']) && !empty($_POST['password'])) {
-    $connection = mysqli_connect($host, $usernameDB, $passwordDB, $database) or die( "Unable to connect");
-    $hashPswd = md5($password);
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $connection = mysqli_connect($host, $usernameDB, $passwordDB, $database) or die( "Unable to Connect");
+    $email = $_POST['email'];
+    $hashPswd = md5($_POST['password']);
     $query = "SELECT * FROM User WHERE Email='$email' AND Password='$hashPswd'";
 
     if($stmt=mysqli_prepare($connection, $query)) {
@@ -34,27 +16,24 @@ if(!empty($_POST['email']) && !empty($_POST['password'])) {
 
         $result = mysqli_query($connection, $query);
         if(mysqli_stmt_num_rows($stmt) == 1) {
-            echo "User '$email' login successfully. ";
-
             while($row = mysqli_fetch_array($result))
             {
                 if($row['UserType'] == "OWNER"){
                     header("Location: owner_function.php/?email=$email");
-                }else if($row['UserType'] == "VISITOR"){
+                }
+                else if($row['UserType'] == "VISITOR"){
                     $visitor_username = $row['Username'];
                     header("Location: visitor_main_page.php?name=$visitor_username");
-                }else if($row['UserType'] == "ADMIN"){
+                }
+                else if($row['UserType'] == "ADMIN"){
                     $admin_username = $row['Username'];
                     header("Location: admin_menu.php/?name=$admin_username");
                 }
             }
         }
         else {
-            echo "Login failed!";
+            echo "Login Failed";
         }   
-    }
-    else {
-        echo "Connection Failed";
     }
 }
 ?>
@@ -62,14 +41,14 @@ if(!empty($_POST['email']) && !empty($_POST['password'])) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>User Login</title>
+    <title>Login</title>
 </head>
 <body>
 
-<form action="login.php" method="post">
-    Email: <input type="text" name="email"><span>* <?php echo $emailErr;?></span><br>
-    Password: <input type="password" name="password"><span>* <?php echo $passwordErr;?></span><br>
-    <input value="Login" type="submit">
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    Email: <input type="text" name="email" value="<?php echo $email; ?>" required><br/><br/>
+    Password: <input type="password" name="password" required><br/><br/>
+    <input value="Login" type="submit"><br/><br/>
 </form>
 
 <a href='register_owner.php'>New Owner Registration</a><br/>
