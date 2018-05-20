221939ccf9b4111e1b80f30aefba8f32430270a7
shimiaoz <36020011+shimiaoz@users.noreply.github.com>
2018-05-20 15:38:23 -0400

    Update login.php

--- login.php
+++ login.php
@@ -5,36 +5,45 @@ require 'dbinfo.php';
 $email = "";
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $connection = mysqli_connect($host, $usernameDB, $passwordDB, $database) or die( "Unable to Connect");
+    $connection = new mysqli($host, $usernameDB, $passwordDB, $database);
+    if ($connection->connect_errno) {
+        //printf("Unable to Connect: %s\n", mysqli_connect_error());
+        echo "Unable to Connect";
+        exit();
+    }
+
     $email = $_POST['email'];
     $hashPswd = md5($_POST['password']);
-    $query = "SELECT * FROM User WHERE Email='$email' AND Password='$hashPswd'";
+    $query = "SELECT * FROM User WHERE Email=? AND Password=?";
 
-    if($stmt=mysqli_prepare($connection, $query)) {
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_store_result($stmt);
+    if($stmt = $connection->prepare($query)) {
+        $stmt->bind_param("ss", $email, $hashPswd);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-        $result = mysqli_query($connection, $query);
-        if(mysqli_stmt_num_rows($stmt) == 1) {
-            while($row = mysqli_fetch_array($result))
-            {
-                if($row['UserType'] == "OWNER"){
-                    header("Location: owner_function.php/?email=$email");
-                }
-                else if($row['UserType'] == "VISITOR"){
-                    $visitor_username = $row['Username'];
-                    header("Location: visitor_main_page.php?name=$visitor_username");
-                }
-                else if($row['UserType'] == "ADMIN"){
-                    $admin_username = $row['Username'];
-                    header("Location: admin_menu.php/?name=$admin_username");
-                }
+        $num_of_rows = $result->num_rows;
+        if($num_of_rows == 1) {
+            $row = $result->fetch_assoc();
+            if($row['UserType'] == "OWNER"){
+                header("Location: owner_function.php?email=$email");
+            }
+            else if($row['UserType'] == "VISITOR"){
+                $visitor_username = $row['Username'];
+                header("Location: visitor_main_page.php?name=$visitor_username");
+            }
+            else if($row['UserType'] == "ADMIN"){
+                $admin_username = $row['Username'];
+                header("Location: admin_menu.php?name=$admin_username");
             }
         }
         else {
             echo "Login Failed";
-        }   
+        }
+
+        $stmt->free_result();
+        $stmt->close();
     }
+    $connection->close();
 }
 ?>
 
