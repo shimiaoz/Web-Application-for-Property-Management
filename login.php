26afc3a6fd2083402dd2ad4ec31a50c565440dc8
shimiaoz <36020011+shimiaoz@users.noreply.github.com>
2018-05-23 00:45:41 -0400

    Update login.php

--- login.php
+++ login.php
@@ -7,83 +7,8 @@ session_start();
 <head>
 <title>Login</title>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="style.css">
 <style>
-    body {
-        font-family: "Times New Roman", Times, serif;
-        font-weight: bold;
-    }
-    .main {
-        position: absolute;
-        text-align: center;
-        border: 1px solid #000000;
-        background-color: #F3F3F3;
-        width: 600px;
-        height: 350px;
-        top: 50%;
-        left: 50%;
-        transform: translateX(-50%) translateY(-50%);
-    }
-    .topBar {
-        border-style: solid;
-        border-width: 0px 0px 1px 0px;
-        border-color: #000000;
-        background-color: #CCCCCC;
-        width: 100%;
-        height: 35px;
-    }
-    .title {
-        padding-top: 15px;
-        padding-bottom: 20px;
-        font-size: 1.8em;
-    }
-    .label-col {
-        float: left;
-        width: 30%;
-        margin-top: 12px;
-        font-size: 1.15em;
-        text-align: right;
-    }
-    .input-col {
-        float: left;
-        box-sizing: border-box;
-        width: 70%;
-        padding-left: 10px;
-        margin-top: 12px;
-        text-align: left;
-    }
-    input[type=text],
-    input[type=password] {
-        width: 72%;
-        border: 0.5px solid;
-        font-size: 1.05em;
-    }
-    .row:after {
-        content: "";
-        display: table;
-        clear: both;
-    }
-    .errMessage {
-        color: #FF0000;
-    }
-    i.fa.fa-exclamation-circle {
-        font-size: 14px;
-        color: #FF0000;
-    }
-    .button {
-        background-color: #CCCCCC;
-        border: 1px solid #000000;
-        border-radius: 4px;
-        padding: 5px 8px;
-        margin: 0px 12px;
-        font-family: "Times New Roman", Times, serif;
-        font-weight: bold;
-        font-size: 1.2em;
-        cursor: pointer;
-    }
-    .button:hover {
-        background-color: #BBBBBB;
-        /*box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);*/
-    }
 </style>
 </head>
 <body>
@@ -93,15 +18,21 @@ require "dbinfo.php";
 
 $email = $errMessage = "";
 
+if (isset($_SESSION["connection_failure_message"]))
+{
+    $errMessage = $_SESSION["connection_failure_message"];
+    unset($_SESSION["connection_failure_message"]);
+}
+
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
     // Connect to database
     $connection = new mysqli($host, $usernameDB, $passwordDB, $database);
     if ($connection->connect_errno)
     {
-        //printf("Unable to Connect: %s\n", mysqli_connect_error());
-        $errMessage = "System unavailable. Please try again later.";
-        exit();
+        //Display error message at the same page when the connection fails
+        $_SESSION["connection_failure_message"] = "System unavailable. Please try again later.";
+        header("Location: " . htmlspecialchars($_SERVER["PHP_SELF"]));
     }
 
     $email = $_POST["email"];
@@ -136,9 +67,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
             }
         }
         else
-        {
-            $errMessage = "<i class='fa fa-exclamation-circle'></i> Login is not successful. Please try signing in again.";
-        }
+            $errMessage = "Login is not successful. Please try signing in again.";
 
         $stmt->free_result();
         $stmt->close();
@@ -149,26 +78,33 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 
 <div class="main">
     <div class="topBar"></div>
-    <div id="loginPage">
-        <div class="title">Welcome to Property Management System</div>
-        <?php echo "<span class=errMessage>" . $errMessage . "</span>"; ?><br/>
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-            <div class="row">
-                <div class="label-col"><label>Email:</label></div>
-                <div class="input-col">
-                    <input type="text" name="email" value="<?php echo $email; ?>" required>
-                </div>
+    <div class="title">Welcome to Property Management System</div>
+    <?php
+        if (!empty($errMessage))
+        {
+            $errMessage = "<i class='fa fa-exclamation-circle'></i> " . $errMessage;
+            echo "<span class='errMessage'>" . $errMessage . "</span>";
+        }
+    ?>
+    <br/>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+        <div class="row">
+            <div class="label-col"><label>Email:</label></div>
+            <div class="input-col">
+                <input type="text" name="email" value="<?php echo $email; ?>" required>
             </div>
-            <div class="row">
-                <div class="label-col"><label>Password:</label></div>
-                <div class="input-col">
-                    <input type="password" name="password" required>
-                </div>
+        </div>
+        <div class="row">
+            <div class="label-col"><label>Password:</label></div>
+            <div class="input-col">
+                <input type="password" name="password" required>
             </div>
-            <br/><br/>
-            <input class="button" type="submit" value="Login">
-        </form>
+        </div>
         <br/>
+        <input class="button" type="submit" value="Login">
+    </form>
+
+    <div class="button-bottom">
         <!--New Owner Registration-->
         <input class="button" type="button" onclick="location.href='register_owner.php';" value="New Owner Registration">
         <!--New Visitor Registration-->
