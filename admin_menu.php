fd0ba289f2399197457e0cd3f453ab58ba421665
shimiaoz <36020011+shimiaoz@users.noreply.github.com>
2018-06-02 00:15:54 -0400

    Update admin_menu.php

--- admin_menu.php
+++ admin_menu.php
@@ -1,21 +1,38 @@
 <?php
 session_start();
-$username = $_GET['name'];
-$_SESSION['adminName'] = $username;
 ?>
+
+<!DOCTYPE html>
 <html>
-<title>Admin Menu</title>
+<head>
+    <title>Administrator Menu</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <style>
+        .main {
+            height: 450px;
+        }
+        .button {
+            margin-top: 12px;
+            width: 55%;
+        }
+    </style>
+</head>
 <body>
 <?php
-echo "<h1>Welcome ".$username. "</h1>";
+$username = $_SESSION["USER"]["Username"];
+echo <<<EOT
+<div class="main">
+    <div class="topBar"></div>
+    <div class="title">Welcome $username</div>
+    <div><input class="button" type="button" onclick="location.href='view_visitors.php';" value="View Visitors"></div>
+    <div><input class="button" type="button" onclick="location.href='view_owners.php';" value="View Owners"></div>
+    <div><input class="button" type="button" onclick="location.href='confirmed_properties.php';" value="View Confirmed Properties"></div>
+    <div><input class="button" type="button" onclick="location.href='unconfirmed_properties.php';" value="View Unconfirmed Properties"></div>
+    <div><input class="button" type="button" onclick="location.href='view_approved_animals_crops.php';" value="View Approved Animals and Crops"></div>
+    <div><input class="button" type="button" onclick="location.href='view_pending_animals_crops.php';" value="View Pending Animals and Crops"></div>
+    <div><input class="button" type="button" onclick="location.href='login.php';" value="Log out"></div>
+</div>
+EOT;
 ?>
-<a href='http://localhost/phase3/view_visitors.php'>View Visitors</a><br>
-<a href='http://localhost/phase3/view_owner.php'>View Owners</a><br/>
-<a href='http://localhost/phase3/confirmed_properties.php'>View Confirmed Properties</a><br/>
-<a href='http://localhost/phase3/unconfirmed_properties.php'>View Unconfirmed Properties</a><br/>
-<a href='http://localhost/phase3/view_approved_animals_crops.php'>View Approved Animals and Crops</a><br/>
-<a href='http://localhost/phase3/view_pending_animals_crops.php'>View Pending Animals and Crops</a><br/>
-<a href='http://localhost/phase3/login.php'>Log out</a>
-
 <body>
 </html>
